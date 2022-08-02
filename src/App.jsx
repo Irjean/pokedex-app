5e034dadbe07f1e12559072cfce8a6c7a95080b7
Spileers Clément <c.spileers@outlook.fr>
2022-08-02 17:04:54 +0200

    Created routing of the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,22 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import PokemonCard from './components/PokemonCard/PokemonCard'
+import Home from './pages/Home/Home'
+import Pokedex from './pages/Pokedex/Pokedex'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='App'>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}>
+              <Route path='/my-pokedex' element={<Pokedex />}/>
+            </Route>
+          </Routes>
+      </Router>
     </div>
   )
 }
