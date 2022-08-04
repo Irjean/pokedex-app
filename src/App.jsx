c235ea2ff4301d8a3bfcbe25a67591d076a4fcd3
Spileers Clément <c.spileers@outlook.fr>
2022-08-04 13:44:50 +0200

    created a route for each pokemon per id

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import './App.css'
 import PokemonCard from './components/PokemonCard/PokemonCard'
 import Home from './pages/Home/Home'
 import Pokedex from './pages/Pokedex/Pokedex'
+import PokemonList from './components/PokemonList/PokemonList'
+import Pokemon from './components/Pokemon/Pokemon'
+
 
 function App() {
 
@@ -13,7 +16,9 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<Home />}>
+              <Route path='/' element={<PokemonList />} />
               <Route path='/my-pokedex' element={<Pokedex />}/>
+              <Route path='/pokemon/:id' element={<Pokemon />} />
             </Route>
           </Routes>
       </Router>
