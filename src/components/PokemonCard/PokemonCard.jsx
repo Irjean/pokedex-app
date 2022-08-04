a33864585f69c60f791049b8a1ad9a08a0d802ae
Spileers Clément <c.spileers@outlook.fr>
2022-08-04 13:47:39 +0200

    linked the card with the data from its props

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux"
+import { Link } from 'react-router-dom'
 import styles from "./PokemonCard.module.css"
+import emptyball from "../../assets/emptyball.png"
+import pokeball from "../../assets/pokeball.png"
+import { pokedexAdded, pokedexRemoved } from '../../services/pokedex.service'
 
-function PokemonCard() {
+function PokemonCard(props) {
+
+  const pokedex = useSelector((store) => store.pokedexReducer)
+
+  function isSaved() {
+    return pokedex.some(i => i.id == props.id);
+  }
+
+  const [saved, setSaved] = useState(isSaved());
+  const dispatch = useDispatch();
+
   return (
+    <Link to={`/pokemon/${props.id}`}>
     <div className={styles.pokemonCard}>
-        <img className={styles.pokemonImg} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" alt="pokémon" />
-        <div className={styles.pokemonName}>Salameche</div>
+        <img className={styles.pokeball} src={saved ? pokeball : emptyball} onClick={(e) => {
+          e.preventDefault();
+          if(!saved){
+            setSaved(true);
+            dispatch(pokedexAdded({id: props.id, name: props.name}));
+          } else {
+            setSaved(false);
+            dispatch(pokedexRemoved({id: props.id}));
+          }
+        }} alt="pokeball" />
+        <img className={styles.pokemonImg} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt="pokémon" />
+        <div className={styles.pokemonName}>{props.name}</div>
     </div>
+    </Link>
   )
 }
 
