533e0ef2135d92cb5d8c66a75e3c0a08073c3dda
Spileers Clément <c.spileers@outlook.fr>
2022-08-07 02:15:14 +0200

    add and remove data from local storage

--- src/services/pokedex.service.js
+++ src/services/pokedex.service.js
@@ -1,8 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+//Création du state et des reducers
 const pokedexSlice = createSlice({
     name: 'pokedex',
-    initialState: [],
+    initialState: localStorage.getItem("pokedex") !== null ? JSON.parse(localStorage.getItem("pokedex")).list : [],
     reducers: {
       pokedexAdded(state, action) {
         state.push({
@@ -19,3 +20,23 @@ const pokedexSlice = createSlice({
   
 export const { pokedexAdded, pokedexRemoved } = pokedexSlice.actions
 export default pokedexSlice.reducer
+
+//Function that add or remove a pokemon from the pokedex in the local storage
+export function addPokedexStorage(id, name){
+  if(localStorage.getItem("pokedex") !== null){
+  let pokedex = JSON.parse(localStorage.getItem("pokedex"));
+  pokedex.list.push({
+    id,
+    name
+  })
+  localStorage.setItem("pokedex", JSON.stringify(pokedex));
+} else {
+  let pokedex = {list: [{id, name}]}
+  localStorage.setItem("pokedex", JSON.stringify(pokedex));
+}}
+
+export function removePokedexStorage(id){
+  let pokedex = JSON.parse(localStorage.getItem("pokedex"));
+  pokedex.list = pokedex.list.filter(i => i.id !== id);
+  localStorage.setItem("pokedex", JSON.stringify(pokedex));
+}
