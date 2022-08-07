3e6ec7ae651b963af13de3f289a4e9071bcb9284
Spileers Clément <c.spileers@outlook.fr>
2022-08-07 02:18:49 +0200

    added a confirmation popup before removing pokemon from pokedex

--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -4,20 +4,39 @@ import { Link } from 'react-router-dom'
 import styles from "./PokemonCard.module.css"
 import emptyball from "../../assets/emptyball.png"
 import pokeball from "../../assets/pokeball.png"
-import { pokedexAdded, pokedexRemoved } from '../../services/pokedex.service'
+import { addPokedexStorage, pokedexAdded, pokedexRemoved, removePokedexStorage } from '../../services/pokedex.service'
 
 function PokemonCard(props) {
 
   const pokedex = useSelector((store) => store.pokedexReducer)
 
+  //checks if the pokemon is already in the pokedex
   function isSaved() {
     return pokedex.some(i => i.id == props.id);
   }
 
   const [saved, setSaved] = useState(isSaved());
+  const [popup, setPopup] = useState(false);
   const dispatch = useDispatch();
 
   return (
+    <>
+    <div className={popup ? styles.pokePopupContainer : styles.close}>
+      <div className={styles.pokePopup}>
+        <p>Remove {props.name.charAt(0).toUpperCase() + props.name.slice(1)} from your pokedex ?</p>
+        <div className={styles.buttonContainer}>
+          <button className={styles.yesButton} onClick={() => {
+            setSaved(false);
+            dispatch(pokedexRemoved({id: props.id}));
+            removePokedexStorage(props.id);
+            setPopup(false)
+          }}>Yes</button>
+          <button className={styles.noButton} onClick={() => {
+            setPopup(false);
+          }}>No</button>
+        </div>
+      </div>
+    </div>
     <Link to={`/pokemon/${props.id}`}>
     <div className={styles.pokemonCard}>
         <img className={styles.pokeball} src={saved ? pokeball : emptyball} onClick={(e) => {
@@ -25,15 +44,16 @@ function PokemonCard(props) {
           if(!saved){
             setSaved(true);
             dispatch(pokedexAdded({id: props.id, name: props.name}));
+            addPokedexStorage(props.id, props.name);
           } else {
-            setSaved(false);
-            dispatch(pokedexRemoved({id: props.id}));
+            setPopup(true);
           }
         }} alt="pokeball" />
         <img className={styles.pokemonImg} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt="pokémon" />
-        <div className={styles.pokemonName}>{props.name}</div>
+        <div className={styles.pokemonName}>{props.name.charAt(0).toUpperCase() + props.name.slice(1)}</div>
     </div>
     </Link>
+    </>
   )
 }
 
